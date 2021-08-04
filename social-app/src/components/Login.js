798cb8b4617c93f15dd23aa79e878c66c19c9850
aman.singh5 <aman.singh@cillabs.com>
2021-08-04 17:33:04 +0530

    Contolled component from react-state

--- social-app/src/components/Login.js
+++ social-app/src/components/Login.js
@@ -3,15 +3,36 @@ import React, { Component } from 'react';
 class Login extends Component {
   constructor(props) {
     super(props);
-    this.emailInputRef = React.createRef();
-    this.passwordInputRef = React.createRef();
+    // // Login using Uncontrolled-Component Method(i.e. getting data from the dom)
+    // this.emailInputRef = React.createRef();
+    // this.passwordInputRef = React.createRef();
+
+    // // Login using Controlled-Component Method(ie. from react state)
+    this.state = {
+      email: '',
+      paswword: '',
+    };
   }
 
-  // Login using Uncontrolled-Component Method(i.e. getting data from the dom)
+  handleEmailChange = (e) => {
+    //console.log(e.target.value);
+    this.setState({
+      email: e.target.value,
+    });
+  };
+
+  handlePasswordChange = (e) => {
+    //console.log(e.target.value);
+    this.setState({
+      password: e.target.value,
+    });
+  };
+
   handleFormSubmit = (e) => {
     e.preventDefault();
-    console.log('this.emailInputRef', this.emailInputRef);
-    console.log('this.passwordInputRef', this.passwordInputRef);
+    // console.log('this.emailInputRef', this.emailInputRef);
+    // console.log('this.passwordInputRef', this.passwordInputRef);
+    console.log('this.state', this.state);
   };
 
   render() {
@@ -22,7 +43,9 @@ class Login extends Component {
           <input
             type="email"
             placeholder="Email"
-            ref={this.emailInputRef}
+            //ref={this.emailInputRef}
+            onChange={this.handleEmailChange}
+            value={this.state.email}
             required
           />
         </div>
@@ -30,7 +53,9 @@ class Login extends Component {
           <input
             type="password"
             placeholder="Password"
-            ref={this.passwordInputRef}
+            //ref={this.passwordInputRef}
+            onChange={this.handlePasswordChange} // this will be called when there will bws ome changes in the input box
+            value={this.state.password}
             required
           />
         </div>
