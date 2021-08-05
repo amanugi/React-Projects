43ac73581016016c6ea48c34b68eec6691d66276
aman.singh5 <aman.singh@cillabs.com>
2021-08-05 11:58:42 +0530

    User login-2

---
+++ social-app/src/helpers/utils.js
@@ -0,0 +1,13 @@
+export function getFormBody(params) {
+  // params object will contain all the property
+  let formBody = []; // an array
+
+  for (let property in params) {
+    let encodedKey = encodeURIComponent(property); // user name => user%20name
+    let encodedValue = encodeURIComponent(params[property]); // aman singh => aman%20singh
+
+    formBody(encodedKey + '=' + encodedValue);
+  }
+
+  return formBody.join('&'); // username=aman&password=123
+}
